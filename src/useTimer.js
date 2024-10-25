8e01c5775f4234d9f22f3b1c6bed15dfc40ee40a
trandat1803 <quocdat@MacprocuaDatne.local>
2024-10-25 10:55:33 +0700

    Dat|finished timer

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,21 +1,31 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [time, setTime] = useState(ini);
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(true);
+  //const [isStart, setIsStart] = useState(false);
+  const active = useRef(null);
+  const refInterval = useRef(0);
 
   const startTimer = () => {
-    "Your code here";
+    if(!isStart.current) {
+      refInterval.current =setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+      isStart.current = true;  //update the ref to indicate the timer is running.
     active.current.disabled = true;
   };
+}
   const stopTimer = () => {
-    "Your code here";
+    clearInterval(refInterval.current);
+    isStart.current = false;
+    active.current.disabled = false;
   };
+
   const resetTimer = () => {
-    "Your code here";
+    clearInterval(refInterval.current);
+    setTime(0);
     active.current.disabled = false;
   };
 
